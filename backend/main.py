4ed3fd3c6180742c4983906fab5e8a59141524e6
khaled <kms_202@hotmail.com>
2026-06-27 23:17:15 +0300

    first commit with backend almost done (no error handling yet)

---
+++ backend/main.py
@@ -0,0 +1,139 @@
+from fastapi import FastAPI, Body
+from typing import Annotated
+from google import genai
+from google.genai import types
+import requests
+import json
+
+gemini_api_key = "AQ.Ab8RN6JPn0k9__PvfdVzICbOrT0QEr0j3QK2rnP054EPzsBKXg"
+token = "3779984fa6a1dcc9471edd030d658461"
+superhero_base_url = f"https://superheroapi.com/api/{token}"
+
+app = FastAPI()
+client = genai.Client(api_key= gemini_api_key)
+
+#open the world cup csv file and read it in bytes
+with open("world_cup.csv", "rb") as f:
+    world_cup_bytes = f.read()
+    
+#prompt engineering of the system prompt (configuring the model)
+system_prompt = """
+You are an assistant that is only supposed to answer questions based on either the fifa world cup or superheros.
+
+Rules:
+- Only call get_worldcup if the user explicitly mentions "world cup" or FIFA World Cup.
+_ Do not call get_worldcup if the user prompt asks about countries in general.
+- Only call get_superhero when the user asks about superheros.
+"""
+
+#gemini LLM will call this function if user prompt asks about a superhero
+superhero_function = {
+    "type": "function",
+    "name": "get_superhero",
+    "description": "extracts the name of the superhero from the user prompt. Calls the superhero api and return back information relevant to the extracted superhero name",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": "string",
+                "description": "The name of the superhero, e.g. spiderman",
+            }
+        },
+        "required": ["name"],
+    }
+}
+
+#gemini LLM will call this function if user prompt is related to the world cup
+worldcup_function = {
+    "type": "function",
+    "name": "get_worldcup",
+    "description": "makes a LLM call that analysis a world cup csv file and return a summary relevant to the user prompt.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "user_prompt": {
+                "type": "string",
+                "description": "the query of what the user is looking for in the csv file"
+            }
+        }
+    }
+}
+
+#calls the SuperHero API and retrieves information about the superhero to query for
+def get_superhero(name):
+    
+    url = f"{superhero_base_url}/search/{name}"
+    
+    try:
+        response = requests.request("GET", url)
+        
+        #for simplicity: only return the first result (later on can print more results if required)
+        superhero_data = json.loads(response.text)["results"][0]
+    
+        #for simplicity: only return name, full-name, place of birth and image url (can add more fields if required)
+        return  {
+            "name": superhero_data["name"],
+            "full-name": superhero_data["biography"]["full-name"],
+            "place-of-birth": superhero_data["biography"]["place-of-birth"],
+            "image": superhero_data["image"]["url"]        
+        }
+    except:
+        return f"I am sorry but I could not find any information about {name}"
+ 
+
+#requests gemini LLM to read the world_cup.csv file and returns a summary of the response based on the user prompt
+def get_worldcup(user_prompt):
+    
+    csv_response = client.models.generate_content(
+        model = "gemini-3.1-flash-lite",
+        config= {
+            "system_instruction": "respond only in text. Summarize the output into a paragraph."
+        },
+        contents = [
+            types.Part.from_bytes(
+                data=world_cup_bytes,
+                mime_type="text/csv",
+            ),
+            user_prompt,
+        ]
+    )
+    
+    return csv_response.text
+    
+
+@app.post("/ask")
+def call_gemini(user_prompt: Annotated[str, Body()]):
+    #make a call to gemini LLM
+    response = client.interactions.create(
+    model = "gemini-3.1-flash-lite",
+    system_instruction = system_prompt,
+    input= user_prompt,
+    tools = [superhero_function, worldcup_function]
+    ) 
+
+    #based on the user prompt, trigger the proper function call
+    for step in response.steps:
+        match step.type:
+            case "model_output":
+                return "I am sorry but i can only answer questions related to superheros or the fifa world cup!"
+            
+            case "function_call":
+                
+                if(step.name == "get_superhero"):
+                    response = get_superhero(**step.arguments)
+                    
+                    return response
+                   
+                elif(step.name == "get_worldcup"):
+                    response = get_worldcup(**step.arguments)
+
+                    return response
+
+
+#TO DO LIST: 
+# 1. create a post endpoint
+# 2. create a very simple front end
+# 3. host it on azure
+# 4. add catch errors for all api calls
+# 5. restrict the llm to only a fixed amount of token. Anything creater than that should return an error
+# 6. add rate limiting to only send specific number of requests to the model in a fixed period of time
