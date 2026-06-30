d92d7fff493eda855e8f90663483a6c788e34f67
khaled <kms_202@hotmail.com>
2026-06-30 15:40:24 +0300

    updated some of the prompt engineering and included a userinferface of the chatbot

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Body
 from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 from google import genai
 from google.genai import types
 import requests
@@ -12,6 +13,14 @@ superhero_base_url = f"https://superheroapi.com/api/{token}"
 app = FastAPI()
 client = genai.Client(api_key= gemini_api_key)
 
+#later on for safety reasons only allow requests from trusted sources and also add credentials
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 #open the world cup csv file and read it in bytes
 with open("world_cup.csv", "rb") as f:
     world_cup_bytes = f.read()
@@ -21,8 +30,9 @@ system_prompt = """
 You are an assistant that is only supposed to answer questions based on either the fifa world cup or superheros.
 
 Rules:
+- Do not answer any opinionated questions such as who is your favorite superhero or who is your favorite world cup team.
 - Only call get_worldcup if the user explicitly mentions "world cup" or FIFA World Cup.
-_ Do not call get_worldcup if the user prompt asks about countries in general.
+_ Do not call get_worldcup if the user prompt asks about countries in general or dates in general.
 - Only call get_superhero when the user asks about superheros.
 """
 
@@ -64,21 +74,52 @@ def get_superhero(name):
     
     url = f"{superhero_base_url}/search/{name}"
     
+    #error handling
     try:
         response = requests.request("GET", url)
-        
+      
         #for simplicity: only return the first result (later on can print more results if required)
-        superhero_data = json.loads(response.text)["results"][0]
+        superhero_data = json.dumps(json.loads(response.text)["results"][0])
     
         #for simplicity: only return name, full-name, place of birth and image url (can add more fields if required)
-        return  {
-            "name": superhero_data["name"],
-            "full-name": superhero_data["biography"]["full-name"],
-            "place-of-birth": superhero_data["biography"]["place-of-birth"],
-            "image": superhero_data["image"]["url"]        
-        }
+        # return  {
+        #     "name": superhero_data["name"],
+        #     "full-name": superhero_data["biography"]["full-name"],
+        #     "place-of-birth": superhero_data["biography"]["place-of-birth"],
+        #     "image": superhero_data["image"]["url"]        
+        # }
+        
+        user_prompt = f"""
+            Here is JSON data for a comic book character:
+            
+            {superhero_data}
+            
+            Create a short engaging biography that includes his name, physical traits, where he was born and data you find relevant in the input data.
+            
+            Return ONLY valid JSON with:
+            -summary: string (2-4 sentences)
+            -image_url: string (must come from input data)
+        """
+        
+        response = client.models.generate_content(
+            model="gemini-3.1-flash-lite",
+            contents=user_prompt,
+            config = types.GenerateContentConfig(
+                response_mime_type="application/json",
+                response_schema={
+                    "type": "object",
+                    "properties": {
+                        "summary": {"type": "string"},
+                        "image_url": {"type": "string"}
+                    },
+                    "required": ["summary", "image_url"]
+                }
+            )
+        )
+        
+        return response.text
     except:
-        return f"I am sorry but I could not find any information about {name}"
+        return f"I am sorry but I could not find any information about {name}  \U0001F615"
  
 
 #requests gemini LLM to read the world_cup.csv file and returns a summary of the response based on the user prompt
@@ -103,6 +144,7 @@ def get_worldcup(user_prompt):
 
 @app.post("/ask")
 def call_gemini(user_prompt: Annotated[str, Body()]):
+    print(user_prompt)
     #make a call to gemini LLM
     response = client.interactions.create(
     model = "gemini-3.1-flash-lite",
@@ -115,19 +157,29 @@ def call_gemini(user_prompt: Annotated[str, Body()]):
     for step in response.steps:
         match step.type:
             case "model_output":
-                return "I am sorry but i can only answer questions related to superheros or the fifa world cup!"
+                return "I am sorry but i can only answer questions related to superheros or the fifa world cup!  \U0001F92D"
             
             case "function_call":
                 
                 if(step.name == "get_superhero"):
+                    print(step.arguments)
                     response = get_superhero(**step.arguments)
-                    
-                    return response
+                  
+                    return {
+                        "name": step.arguments["name"],
+                        "message": response,
+                        "source": "superhero API",
+                        "source_url": "https://superheroapi.com/"
+                    }
                    
                 elif(step.name == "get_worldcup"):
                     response = get_worldcup(**step.arguments)
 
-                    return response
+                    return {
+                        "message": response,
+                        "source": "Football - FIFA World Cup, 1930 - 2026",
+                        "source_url": "https://www.kaggle.com/datasets/piterfm/fifa-football-world-cup?resource=download&select=world_cup.csv"
+                    }
 
 
 #TO DO LIST: 
---
+++ frontend/src/App.tsx
@@ -0,0 +1,176 @@
+
+import { Message } from '@mui/icons-material';
+import './App.css'
+import {ChatBox, createEchoAdapter} from "@mui/x-chat";
+import type { ChatAdapter, ChatMessage } from '@mui/x-chat/headless';
+
+function App() {
+
+  const initialConversations = [
+    {
+      id: "demo-conversation",
+      participants: [
+        {
+          displayName: 'user (test)',
+          id: 'user-1',
+          role: 'user' as const,
+          avatatUrl: 'https://img.magnific.com/premium-vector/portrait-young-arab-man-with-beard-middle-eastern-ethnicity-businessman-flat-illustration_1224831-38.jpg?semt=ais_hybrid&w=740&q=80'
+      
+        },
+        {
+          displayName: 'AI assistant (test)',
+          id: 'assistant-1',
+          role: 'assistant' as const,
+          avatarUrl: 'https://img.magnific.com/premium-vector/portrait-young-arab-man-with-beard-middle-eastern-ethnicity-businessman-flat-illustration_1224831-38.jpg?semt=ais_hybrid&w=740&q=80'
+        }
+      ]
+    }
+  ]
+
+  const minimalMessages: ChatMessage[] = [
+    {
+      id: 'starter-msg-1',
+      conversationId: 'demo-conversation',
+      role: 'assistant',
+      author: {
+        id: 'assistant-1',
+        displayName: 'AI assistant (test)',
+        role: 'assistant'
+      },
+      parts: [{type: 'text' as const, text: "Hello! I am your AI chatbot  \u{1F916}\n\nI am happy to answer you with any questions regarding superheros \u{1F9B8} or the FIFA world cup \u{26BD}\n\nHow may I help you? \u{1F60A}"}],
+
+    }
+  ]
+
+  //since the current backend sends a non-streaming response we have to implment an adapter with "fake" streaming
+  //code snippet from: https://mui.com/x/react-chat/backend/building-an-adapter/
+  const adapter: ChatAdapter = {
+    async sendMessage({ message, signal }) {
+      return new ReadableStream({
+        async start(controller) {
+          const response = await fetch('http://127.0.0.1:8000/ask', {
+            method: 'POST',
+            body: JSON.stringify(
+              message.parts[0]?.type === 'text' ? message.parts[0].text : '',
+            ),
+            signal,
+          });
+  
+          const reader = response.body!.getReader();
+          const decoder = new TextDecoder();
+          const messageId = `msg-${Date.now()}`;
+  
+          controller.enqueue({ type: 'start', messageId });
+          controller.enqueue({ type: 'text-start', id: 'text-1' });
+  
+          try {
+            while (true) {
+              var finalMessage = "";
+              const { done, value } = await reader.read();
+              if (done) break;
+
+              const text = JSON.parse(decoder.decode(value, { stream: true }));
+           
+              //can't find the super hero
+              if(text.message == `I am sorry but I could not find any information about ${text.name}  😕`) {
+                finalMessage = text.message
+              }
+
+              //either superhero or world cup is called
+              else if(typeof text == "object") {
+                const modelMessage = text.message;
+                const source = text.source;
+                const source_url = text.source_url;
+
+                if(source == "superhero API") {
+                  const superHeroName  = text.name;
+                  const superHero_data = JSON.parse(modelMessage);
+                  const biography = superHero_data.summary;
+                  const image_url = superHero_data.image_url;
+
+                  finalMessage = `${biography}\n\n**source:** [${source}](${source_url})`
+                  
+                  //add superHero image to response
+                  controller.enqueue({
+                    type: 'file',
+                    id: `img-${Date.now()}`,
+                    mediaType: 'image/jpeg',
+                    url: `${image_url}`,
+                    filename: `show a picture of ${superHeroName}`
+                  });
+                }
+
+                else {
+                  finalMessage = `${modelMessage}\n\n\n**source:** [${source}](${source_url})`
+                }
+          
+              }
+              
+              //response for unrelated queries that the model isn't supposed to answer
+              else {
+                finalMessage = text
+              }
+     
+            
+              controller.enqueue({ type: 'text-delta', id: 'text-1', delta: finalMessage });
+            }
+            controller.enqueue({ type: 'text-end', id: 'text-1' });
+            controller.enqueue({ type: 'finish', messageId });
+          } catch (error) {
+            controller.enqueue({ type: 'text-end', id: 'text-1' });
+            controller.enqueue({ type: 'abort', messageId });
+          } finally {
+            controller.close();
+          }
+        },
+      });
+    },
+  };
+
+
+  return (
+    <>
+      <ChatBox
+        initialActiveConversationId='demo-conversation'
+        initialConversations={initialConversations}
+        initialMessages={minimalMessages}
+        density = 'comfortable'
+        adapter = {adapter}
+        features = {{
+          attachments: false,
+          conversationHeader: false,
+        }}
+        sx = {{
+          height: 800,
+          border:'1px solid',
+          borderColor: 'divider',
+          borderRadius: 1,
+
+          "& .MuiChatMessage-bubble p": {
+            marginBottom: "12px"
+          },
+
+          "& .MuiChatMessage-filePreview": {
+            display: "none"
+          }
+        }}
+        slotProps={{
+          messageContent: {
+            style: {
+              textAlign: "left",
+            }
+          },
+          messageAuthorName: (ownerState) => ({
+            style: {
+              textAlign: ownerState.authorRole === "assistant" ? "left" : "right",
+              whiteSpace: "pre-wrap"
+            }
+          }),
+        }}
+      />
+
+    </>
+  )
+}
+
+export default App
