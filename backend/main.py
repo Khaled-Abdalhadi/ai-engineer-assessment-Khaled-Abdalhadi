897c86f1597692c71fa4268d6dd65aabfc0c7ee1
khaled <kms_202@hotmail.com>
2026-07-01 16:44:25 +0300

    included custom error handlers fro gemini and /ask endpoit

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,21 @@
-from fastapi import FastAPI, Body
-from typing import Annotated
+from fastapi import FastAPI, Body, HTTPException, Request
+from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError
+from typing import Annotated
 from google import genai
-from google.genai import types
+from google.genai import types, errors
+from google.genai._gaos.lib.compat_errors import AuthenticationError
 import requests
 import json
 
-gemini_api_key = "AQ.Ab8RN6JMj_OFTJmemQ0zSafNpGzE-g_BDWO5EYsTIliTQaECQA"
+gemini_api_key = "AQ.Ab8RN6LAKgtQGpg5JTQ4ogBk3JI_p8TAUY1iIZUnVGgkMK5tgA"
 token = "3779984fa6a1dcc9471edd030d658461"
 superhero_base_url = f"https://superheroapi.com/api/{token}"
 
+
 app = FastAPI()
+
 client = genai.Client(api_key= gemini_api_key)
 
 #later on for safety reasons only allow requests from trusted sources and also add credentials
@@ -80,15 +85,8 @@ def get_superhero(name):
       
         #for simplicity: only return the first result (later on can print more results if required)
         superhero_data = json.dumps(json.loads(response.text)["results"][0])
-    
-        #for simplicity: only return name, full-name, place of birth and image url (can add more fields if required)
-        # return  {
-        #     "name": superhero_data["name"],
-        #     "full-name": superhero_data["biography"]["full-name"],
-        #     "place-of-birth": superhero_data["biography"]["place-of-birth"],
-        #     "image": superhero_data["image"]["url"]        
-        # }
-        
+
+        #make another call to gemini to summarize the superhero data returned from the API
         user_prompt = f"""
             Here is JSON data for a comic book character:
             
@@ -140,47 +138,75 @@ def get_worldcup(user_prompt):
     )
     
     return csv_response.text
+
+#a customized request validation error handler to just send back a string of the error message
+@app.exception_handler(RequestValidationError)
+async def invalid_user_prompt_exception_handler(request: Request, error: RequestValidationError):
+    return PlainTextResponse(content = "Invalid user prompt, only strings are allowed!", status_code=422)
+
+#a customized http exception handler to only return plain text
+@app.exception_handler(HTTPException)
+async def http_custom_exception_handler(request: Request, error: HTTPException):
+    return PlainTextResponse(content= error.detail, status_code=error.status_code)
     
 
 @app.post("/ask")
 def call_gemini(user_prompt: Annotated[str, Body()]):
-    print(user_prompt)
-    #make a call to gemini LLM
-    response = client.interactions.create(
-    model = "gemini-3.1-flash-lite",
-    system_instruction = system_prompt,
-    input= user_prompt,
-    tools = [superhero_function, worldcup_function]
-    ) 
+    try:
+        #make a call to gemini LLM
+        response = client.interactions.create(
+        model = "gemini-3.1-flash-lite",
+        system_instruction = system_prompt,
+        input= user_prompt,
+        tools = [superhero_function, worldcup_function]
+        )
+        print(response.status)
 
-    #based on the user prompt, trigger the proper function call
-    for step in response.steps:
-        match step.type:
-            case "model_output":
-                return "I am sorry but i can only answer questions related to superheros or the fifa world cup!  \U0001F92D"
-            
-            case "function_call":
+        #based on the user prompt, trigger the proper function call
+        for step in response.steps:
+            match step.type:
+                case "model_output":
+                    return "I am sorry but i can only answer questions related to superheros or the fifa world cup!  \U0001F92D"
                 
-                if(step.name == "get_superhero"):
-                    print(step.arguments)
-                    response = get_superhero(**step.arguments)
-                  
-                    return {
-                        "name": step.arguments["name"],
-                        "message": response,
-                        "source": "superhero API",
-                        "source_url": "https://superheroapi.com/"
-                    }
-                   
-                elif(step.name == "get_worldcup"):
-                    response = get_worldcup(**step.arguments)
+                case "function_call":
+                    
+                    if(step.name == "get_superhero"):
+                        print(step.arguments)
+                        response = get_superhero(**step.arguments)
+                    
+                        return {
+                            "name": step.arguments["name"],
+                            "message": response,
+                            "source": "superhero API",
+                            "source_url": "https://superheroapi.com/"
+                        }
+                    
+                    elif(step.name == "get_worldcup"):
+                        response = get_worldcup(**step.arguments)
 
-                    return {
-                        "message": response,
-                        "source": "Football - FIFA World Cup, 1930 - 2026",
-                        "source_url": "https://www.kaggle.com/datasets/piterfm/fifa-football-world-cup?resource=download&select=world_cup.csv"
-                    }
-
+                        return {
+                            "message": response,
+                            "source": "Football - FIFA World Cup, 1930 - 2026",
+                            "source_url": "https://www.kaggle.com/datasets/piterfm/fifa-football-world-cup?resource=download&select=world_cup.csv"
+                        }
+    
+    #add specific gemini error handling here later                    
+    except errors.APIError as e:
+        print("GenAI API error occured!")
+        
+    #if gemini failed to catch error or if we have general error
+    except Exception as e:
+        
+        ##identify the error
+        print(e)
+        
+        #error triggered when gemini authentication fails (not sure why i cant use it in the above exception)
+        if isinstance(e, AuthenticationError):
+            raise HTTPException(status_code = 401, detail = f"Gemini API Error: {e.body[0]["error"]["message"]}")
+        
+        
+            
+    
 
 #TO DO LIST: 
 # 1. create a post endpoint
